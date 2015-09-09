e3dada2289b5dd93794cbd92ace30d5cbdd13309
Andrew Haupt & KellenKolbeck <student@epicodus.com>
2015-09-09 11:18:32 -0700

    Add functionality for all cases.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
 var pigLatin = function(string) {
-  var result = string + "ay"
-  return result
+  string = string.toLowerCase()
+  var words = string.split(" ")
+  var vowels = /[aeiou]/
+  var allWords = []
+  words.forEach(function(word) {
+    if (vowels.test(word[0])) {
+        word = word + "ay"
+        allWords.push(word)
+    }  else {
+        var consonants = /[bcdfghjklmnp(qu)rstvwxyz]+/
+        var firstConsonants = consonants.exec(word)
+        word = word.replace(firstConsonants[0], "")
+        word = word + firstConsonants[0] + "ay"
+        allWords.push(word)
+    }
+   })
+   return allWords.join(" ")
 }
+
+
+$(document).ready(function() {
+  $("form#pig-latin").submit(function(event) {
+    var string = $("input#string").val();
+    var result = pigLatin(string);
+
+    $("#translated").text(result);
+    $('#result').show()
+  event.preventDefault()
+});
+});
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,20 @@ describe('pigLatin', function() {
   it("adds 'ay' to words that begin with a vowel", function () {
     expect(pigLatin("andrew")).to.equal("andreway")
   });
+
+  it("adds 'ay' plus the first letter to words that begin with a consonant", function () {
+    expect(pigLatin("taco")).to.equal("acotay")
+  });
+
+  it("adds 'ay' plus the letters before a vowel to the end of that word", function () {
+    expect(pigLatin("street")).to.equal("eetstray")
+  });
+
+  it("handles special case of qu", function () {
+    expect(pigLatin("squeak")).to.equal("eaksquay")
+  });
+
+  it("adds appropriate letters plus 'ay' to each word in a sentence", function () {
+    expect(pigLatin("The mouse squeaks")).to.equal("ethay ousemay eakssquay")
+  });
 });
